2b46451258f4c91bf9666937089dccbfdd45ed3c
JonathanLeme <72026020+JonathanLeme@users.noreply.github.com>
2021-11-04 16:36:43 -0300

    Selecionando Elementos

---
+++ M02/02-selecionando_elementos_4funcoes.js
@@ -0,0 +1,7 @@
+document.getElementById('exemplo').innerHTML = 'Raphael'
+
+document.getElementsByClassName('lista')[1].innerHTML = 'Item alterado!'
+
+document.getElementsByTagName('button')[0].innerHTML = 'CLIQUE'
+
+document.getElementsByName('email')[0].innerHTML = ''
