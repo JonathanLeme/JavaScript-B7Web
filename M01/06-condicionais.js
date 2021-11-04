cb091495a7d796caa6641f4cc47f9e5448dd9eef
JonathanLeme <72026020+JonathanLeme@users.noreply.github.com>
2021-11-03 21:00:54 -0300

    Begins JavaScript

---
+++ M01/06-condicionais.js
@@ -0,0 +1,27 @@
+var hora = 15;
+
+if ( hora < 12 ) {
+    console.log("Good morning")
+} else if ( hora < 18 ) {
+    console.log("Good afternoon")
+} else if ( hora <= 23 ) {
+    console.log("Good evening")
+}
+
+/* 
+
+Exemplo de duas condicionais
+
+if ( hora >= 12 && <= 18 ) {
+    console.log("Good afternoon")
+}
+
+if ( hora == 12 || == 18 ) {
+    console.log("Você está na hora do rush.")
+}
+
+O síbolo || significa "OU".
+
+*/
+
+
