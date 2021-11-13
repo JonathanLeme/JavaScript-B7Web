2d44f50dae00a93242c68fe3cc59526333d005d6
JonathanLeme <72026020+JonathanLeme@users.noreply.github.com>
2021-11-12 21:46:51 -0300

    Dimensões

---
+++ M02/12-dimensoes.js
@@ -0,0 +1,23 @@
+document.querySelector('.texto').offsetHeight
+
+/*  */
+
+document.querySelector('.texto').offsetWidth
+
+/*  */
+
+document.querySelector('.texto').clientHeight
+
+/*  */
+
+document.querySelector('.texto').clientWidth
+
+/*  */
+
+document.querySelector('.texto').scrollHeight
+
+/*  */
+
+document.querySelector('.texto').scrollWidth
+
+/*  */
