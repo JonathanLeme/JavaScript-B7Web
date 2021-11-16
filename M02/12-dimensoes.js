63d1ac879946ff80e95a8b7f923a17c5d7a56ae2
JonathanLeme <72026020+JonathanLeme@users.noreply.github.com>
2021-11-15 21:39:05 -0300

    Update

--- M02/12-dimensoes.js
+++ M02/12-dimensoes.js
@@ -1,3 +1,21 @@
+function subirTela() {
+    window.scrollTo(0,0);
+}
+
+/* O "behavior" que o professor fez na aula não funcionou. Verificar mudanças. */
+
+function decidirBotaoScroll() {
+    if(window.scrollY === 0) {
+    document.querySelector('.scrollbutton').style.display = 'none';
+} else {
+    document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
+
+window.addEventListener('scroll', decidirBotaoScroll)
+
+/* Não funcionou ocultar e aparecer. Verificar! */
+
 document.querySelector('.texto').offsetHeight
 
 /*  */
@@ -21,3 +39,30 @@ document.querySelector('.texto').scrollHeight
 document.querySelector('.texto').scrollWidth
 
 /*  */
+
+document.querySelector('.texto').scrollTop
+
+/*  */
+
+document.querySelector('.texto').scrollLeft
+
+/*  */
+
+window.scrollY
+
+/* Vertical tela inteira */
+
+window.scrollX
+
+/* Horizontal tela inteira */
+
+document.querySelector('.texto').scrollTo();
+
+/*  */
+
+window.scrollTo();
+
+/*  */
+
+
+
