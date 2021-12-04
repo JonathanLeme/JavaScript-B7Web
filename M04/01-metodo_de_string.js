844fa8496466e25db97600e0c28820ba29fef6cf
JonathanLeme <72026020+JonathanLeme@users.noreply.github.com>
2021-12-03 22:03:35 -0300

    Métodos de Strings

---
+++ M04/01-metodo_de_string.js
@@ -0,0 +1,21 @@
+let nome = 'Jonathan Leme da Rocha';
+
+let resultado = '';
+
+if(nome.indexOf('Raphael') > -1) {
+    resultado = 'Achou!';
+} else {
+    resultado = 'Não achou!';
+}
+
+console.log(resultado);
+
+/* 
+
+Função de string apresentadas:
+
+nome.length - mostra quantas caracteres existem.
+nome.indexOf(' ') - usado para localizar um palavra ou posição.
+
+
+*/
