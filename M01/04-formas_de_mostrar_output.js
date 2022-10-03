46a60886d3d3d23524fa09482fb6ae7064bbdf77
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-03 13:34:34 -0300

    Begin JavaScript

--- M01/04-formas_de_mostrar_output.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("titulo").innerHTML = "How do you do?"
-
-document.write("I'm fine. And you?")
-
-window.alert("Mensagem de Janela")
-
-console.log("A mensagem que eu inserir aqui aparecerá no console.")
