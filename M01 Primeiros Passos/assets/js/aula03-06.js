5ee1074893d2b0312adb8922628e750e6c195c72
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-04 20:40:02 -0300

    Update
    Studyng JavaScript

---
+++ M01 Primeiros Passos/assets/js/aula03-06.js
@@ -0,0 +1,52 @@
+/* 
+    *AULA 03 - Inserindo o Arquivo JS na Página
+    - "alert" é usado para exibir uma alerta ou comando antes de iniciar a página. 
+*/
+
+alert ("Hello World")
+
+/* 
+    *AULA 04 - Hello World
+    - "console.log" é usado para exibir algo ou resultado no console do "Dev Tools". 
+*/
+
+console.log ("Hello World 3.0")
+
+/* 
+    *AULA 05 - Variáveis
+    - "let" é usado para criar uma varávevl;
+    - Sempre usamos ("") para digitar texto;
+    - Números não é necessário usar ("").
+*/
+
+let estado = "Natal-RN"
+let cidade = "Macaíba"
+
+console.log (estado)
+console.log (cidade)
+
+/* *AULA 06 - Tipos de Dados */
+
+/* "string" é usado para textos. Sempre usar (""). */
+
+let nome = "Jonathan"
+
+/* "number" é usado para números. O padrão internacional é usado com o ponto (.). Vírgula não é aceitado. */
+
+let idade = 0.33
+
+/* "boolean" usado para (sim/não, verdadeiro/falso, on/off), ou seja, é um ou outro. */
+
+let logado = true
+
+/* "array" é usado  para listas. Perceba que usamos [Colchetes] entre a lista. */
+
+let lista = ["farinha", "leite", "ovos", "fermento"]
+
+/* "objeto" é parecido com uma lista (array), mas traz uma referência também. */
+
+let nomeCOmpleto = {nome: "Jonathan", idade: 90 }
+
+/* O código abaixo é usado para verifica o tipo de dado. */
+
+console.log(typeof nome) /* usei o "nome" como exemplo. */
