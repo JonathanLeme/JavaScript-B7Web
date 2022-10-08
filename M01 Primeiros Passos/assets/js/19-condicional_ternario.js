275a0b99b34a38fffc49563ada4ed786906a556e
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-08 12:48:07 -0300

    Update

---
+++ M01 Primeiros Passos/assets/js/19-condicional_ternario.js
@@ -0,0 +1,16 @@
+let isMember = false;
+
+// Exemplo de "Condicional Ternário" entre paranteses.
+let shipping = (isMember ? 2 : 10);
+
+// Outro exemplo de "Condicional Ternário" entre paranteses.
+console.log(isMember ? 'Você é membro' : 'Você não é membro');
+console.log("Frente: " + shipping);
+
+let age = 60;
+
+// Outro exemplo de "Condicional Ternário" entre paranteses.
+// OBS: não é obrigado usar parenteses. É apenas uma forma de organização.
+let isAdult = ((age >= 18 && age < 60) ? 'Você é um adulto' : 'Você não é um adulto');
+
+console.log(isAdult);
---
+++ M01 Primeiros Passos/assets/js/20-switch.js
@@ -0,0 +1,23 @@
+let profession = "Professor"
+
+console.log("Profissão: " + profession)
+
+switch (profession) {
+
+    case 'bombeiro' :
+        console.log('Sua camisa será VERMELHA');
+        break;
+
+    case 'policial' :
+        console.log('Sua camisa será AZUL');
+        break;
+
+    case 'fiscal' :
+        console.log('Sua camisa será VERDE');
+        break;
+
+    default:
+        console.log('Sua camisa será PRETA');
+        break
+
+}
