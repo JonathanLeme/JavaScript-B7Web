3527c11d082de8b862b454b5f8b416fc9be4d154
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-14 12:59:24 -0300

    Update

---
+++ M02 JavaScript Básico/assets/js/20-dando_loop_em_arrays.js
@@ -0,0 +1,23 @@
+let cores = ['Black', 'White', 'Blue', 'Red'];
+
+cores.push('Green');
+
+/* 
+    1° Forma    
+
+    for (let n = 0; n < cores.length; n++) {
+        console.log(cores[n]);
+    } 
+*/
+
+/*
+    2° Forma
+
+    for (let i in cores) {
+        console.log(cores[i]);
+    } 
+*/
+
+for (let cor of cores) {
+    console.log(cor);
+}
