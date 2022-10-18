7d24d2218a41b6af2cded0db9c767a66cd4e4825
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-18 12:57:29 -0300

    Update
    Learning with JavaScript

--- M02 JavaScript Básico/assets/js/20-dando_loop_em_arrays.js
+++ M02 JavaScript Básico/assets/js/20-dando_loop_em_arrays.js
@@ -8,14 +8,18 @@ cores.push('Green');
     for (let n = 0; n < cores.length; n++) {
         console.log(cores[n]);
     } 
-*/
 
-/*
     2° Forma
 
     for (let i in cores) {
         console.log(cores[i]);
     } 
+
+    3° Forma
+
+    for (let cor of cores) {
+        console.log(cor);
+    }
 */
 
 for (let cor of cores) {
---
+++ M02 JavaScript Básico/assets/js/21-loop_while.js
@@ -0,0 +1,16 @@
+/* 
+    Como seria no "loop for"
+
+    for(let numero = 0; numero <= 10; numero++) {
+        console.log(`O número da vez é ${numero}`);
+    } 
+*/
+
+// COmo é no "loop while"
+
+let numero = 0;
+
+while (numero <= 10) {
+    console.log(`O número da vez é ${numero}`);
+    numero++;
+}
