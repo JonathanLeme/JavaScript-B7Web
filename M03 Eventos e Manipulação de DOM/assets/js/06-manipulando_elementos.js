79779c26b407a31a911f84bf654af82607e09431
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-25 13:25:49 -0300

    Update
    To play

---
+++ M03 Eventos e Manipulação de DOM/assets/js/06-manipulando_elementos.js
@@ -0,0 +1,52 @@
+// "append" usado para adicionar/acrescentar textos sem alterar/substituir o já exitente.
+
+/* 
+    function clicou() {
+        const teste = document.querySelector('#teste1');
+        const ul = teste.querySelector('ul');
+
+        ul.children[0].append(" (alterado)");
+    }  
+*/ 
+
+// Adicionando um novo elemento com "innerHTML". Apaga todo o conteúdo e recoloca novamenta + o item adicional. Não é a melhor forma para adiconar algo.
+
+/* 
+    function clicou() {
+        const teste = document.querySelector('#teste1');
+        const ul = teste.querySelector('ul');
+
+        ul.innerHTML += "<li>Algum item adicionado!</li>";
+    } 
+*/    
+
+// Forma correta/efetiva de adicionar um novo elemento com "appendChild". O mesmo apenas acrescenta e não remove todo o conteúdo para recolacar como o "innerHTML".
+
+/* 
+    function clicou() {
+        const teste = document.querySelector('#teste1');
+        const ul = teste.querySelector('ul');
+
+        let newLi = document.createElement("li");
+        newLi.innerHTML = "Algum item adicionado!"
+
+        ul.appendChild(newLi);
+    }
+*/
+
+/* 
+"prepend" é o inverso de 'appendChild'. 
+"appendCHild" ou apenas "append" adiciona no fim da lista;
+"prepend" adiciona no começo da lista.
+*/
+
+
+    function clicou() {
+        const teste = document.querySelector('#teste1');
+        const ul = teste.querySelector('ul');
+
+        let newLi = document.createElement("li");
+        newLi.innerHTML = "Algum item adicionado!"
+
+        ul.prepend(newLi);
+    }
---
+++ M03 Eventos e Manipulação de DOM/assets/js/07-manipulando_elementos.js
@@ -0,0 +1,16 @@
+// "after" and "before"
+
+    function clicou() {
+        const teste = document.querySelector('#teste1');
+        const ul = teste.querySelector('ul');
+
+        let newUl = document.createElement('ul');
+
+        for (let i = 0; i < 5; i++) {
+            let newLi = document.createElement('li');
+            newLi.innerHTML = "Item add " + (i + 1);
+            newUl.append(newLi);
+        }
+
+        ul.after(newUl);
+    }  
