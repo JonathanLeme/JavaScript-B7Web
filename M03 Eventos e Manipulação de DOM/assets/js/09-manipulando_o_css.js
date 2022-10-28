d5ef5a95590473c6543b84af2e99939a97e43a84
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-10-28 13:20:48 -0300

    Update

---
+++ M03 Eventos e Manipulação de DOM/assets/js/09-manipulando_o_css.js
@@ -0,0 +1,9 @@
+// Manipulando CSS pelo JS.
+
+    function clicou() {
+
+        const li = document.querySelector('li');
+
+        li.style.backgroundColor = '#00F';
+        li.style.color = '#0F0';
+    }
