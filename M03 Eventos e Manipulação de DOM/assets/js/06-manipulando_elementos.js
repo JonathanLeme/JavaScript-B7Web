01fc55e3891c0d0126929e40bc611d32501b9b22
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-11-01 13:52:38 -0300

    Update

--- M03 Eventos e Manipulação de DOM/assets/js/06-manipulando_elementos.js
+++ M03 Eventos e Manipulação de DOM/assets/js/06-manipulando_elementos.js
@@ -11,18 +11,15 @@
 
 // Adicionando um novo elemento com "innerHTML". Apaga todo o conteúdo e recoloca novamenta + o item adicional. Não é a melhor forma para adiconar algo.
 
-/* 
     function clicou() {
         const teste = document.querySelector('#teste1');
         const ul = teste.querySelector('ul');
 
         ul.innerHTML += "<li>Algum item adicionado!</li>";
     } 
-*/    
 
 // Forma correta/efetiva de adicionar um novo elemento com "appendChild". O mesmo apenas acrescenta e não remove todo o conteúdo para recolacar como o "innerHTML".
 
-/* 
     function clicou() {
         const teste = document.querySelector('#teste1');
         const ul = teste.querySelector('ul');
@@ -32,14 +29,12 @@
 
         ul.appendChild(newLi);
     }
-*/
 
 /* 
-"prepend" é o inverso de 'appendChild'. 
-"appendCHild" ou apenas "append" adiciona no fim da lista;
-"prepend" adiciona no começo da lista.
+    "prepend" é o inverso de 'appendChild'. 
+    "appendCHild" ou apenas "append" adiciona no fim da lista;
+    "prepend" adiciona no começo da lista.
 */
-
 
     function clicou() {
         const teste = document.querySelector('#teste1');
--- M03 Eventos e Manipulação de DOM/assets/js/10-trabalhando_com_classes.js
+++ M03 Eventos e Manipulação de DOM/assets/js/10-trabalhando_com_classes.js
@@ -1,6 +1,59 @@
-// 
+// "classList" puxa a lista de classe que tenho naquele elemento;
 
-function clicou() {
+    function clicou() {
+        const button = document.querySelector('button');
 
-    
-}
+        console.log(button.classList);
+        
+    }
+
+/* 
+    "classList.add" para adicionar uma classe;
+    "classList.remove" para remover uma classe;
+ */
+
+    function clicou() {
+        const button = document.querySelector('button');
+
+        button.classList.add('green');
+        
+    }
+
+// adicionando e removendo entre 'verde' e 'azul';
+
+    function clicou() {
+        const button = document.querySelector('button');
+
+        if (button.classList.contains('blue')) {
+            button.classList.remove('blue');
+            button.classList.add('green')
+        } else {
+            button.classList.add('blue');
+            button.classList.remove('grenn');
+        }      
+    }
+
+// Forma resumida de adicionar e remover uma classe usando "toggle";
+
+    function clicou() {
+        const button = document.querySelector('button');
+
+        button.classList.toggle('azul')     
+    }
+
+// "replace" usadado para subtituir uma classe;
+
+    function clicou() {
+        const button = document.querySelector('button');
+
+        button.classList.replace('azul', 'yellow')     
+    }
+
+// "replace" usadado para subtituir uma classe;
+
+    function clicou() {
+        const button = document.querySelector('button');
+
+        if (button.classList.contains('azul'))
+        button.classList.replace('azul', 'yellow')     
+    }
