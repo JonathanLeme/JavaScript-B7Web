3dbae271a27671aed3a4afb8372a20ce4bd14289
Jonathan Leme <jonathan.leme.rocha@gmail.com>
2022-11-14 13:56:16 -0300

    Update

---
+++ M07 Manipulações, Arrow, Ajax, etc/assets/js/18-desconstruindo_arrays.js
@@ -0,0 +1,15 @@
+let info = [ 'Jonathan Leme', 'Jonathan', 'Leme', '@jonathan.lrocha']
+
+let [ nomeCompleto, nome, sobrenome, instagram ] = info;
+
+console.log(nomeCompleto, nome, sobrenome, instagram)
+
+/* 
+    Caso queira omitir algum objeto deve-se seimplesmente deixa os espaço em branco depois da vírgula. Exemplo: console.log(nomeCompleto, , , instagram);
+    --
+
+    Uma outra forma de desconsturir um array: let [nome, sobreno] = ['Jonathan', 'Leme'];
+    --
+
+    
+*/
